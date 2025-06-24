e5de26dcf2dafffddc46c49f436ddca2932a85e0
prakash-dev-code <sahuprakash643@gmail.com>
2025-06-25 02:01:37 +0530

    admin logic added

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -2,57 +2,27 @@
 import { useState, useEffect } from 'react';
 import '../css/euclid-circular-a-font.css';
 import '../css/style.css';
-import Header from '../../components/Header';
-import Footer from '../../components/Footer';
 
-import { ModalProvider } from '../context/QuickViewModalContext';
-import { CartModalProvider } from '../context/CartSidebarModalContext';
 import { ReduxProvider } from '@/redux/provider';
-import QuickViewModal from '@/components/Common/QuickViewModal';
-import CartSidebarModal from '@/components/Common/CartSidebarModal';
-import { PreviewSliderProvider } from '../context/PreviewSliderContext';
-import PreviewSliderModal from '@/components/Common/PreviewSlider';
-
-import ScrollToTop from '@/components/Common/ScrollToTop';
 import PreLoader from '@/components/Common/PreLoader';
-import AuthInitializer from '@/components/Auth/AuthInitializer';
-import { Toaster } from 'react-hot-toast';
-import CartInitializer from '@/components/Auth/CartInitializer';
+import { InnerLayout } from './innerLayout';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  const [loading, setLoading] = useState<boolean>(true);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     setTimeout(() => setLoading(false), 1000);
   }, []);
 
   return (
-    <html lang="en" suppressHydrationWarning={true}>
+    <html lang="en" suppressHydrationWarning>
       <body>
         {loading ? (
           <PreLoader />
         ) : (
-          <>
-            <ReduxProvider>
-              <CartInitializer />
-              <AuthInitializer />
-              <CartModalProvider>
-                <ModalProvider>
-                  <PreviewSliderProvider>
-                    <Header />
-                    {children}
-
-                    <QuickViewModal />
-                    <CartSidebarModal />
-                    <PreviewSliderModal />
-                  </PreviewSliderProvider>
-                </ModalProvider>
-                <Footer />
-              </CartModalProvider>
-            </ReduxProvider>
-            <ScrollToTop />
-            <Toaster reverseOrder={false} />
-          </>
+          <ReduxProvider>
+            <InnerLayout>{children}</InnerLayout>
+          </ReduxProvider>
         )}
       </body>
     </html>
--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,23 +1,23 @@
 // src/lib/axios.ts
-import axios from "axios";
-import toast from "react-hot-toast";
+import axios from 'axios';
+import toast from 'react-hot-toast';
 
 const axiosInstance = axios.create({
-  // baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
-  baseURL: process.env.NEXT_PUBLIC_API_LOCAL_URL,
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  // baseURL: process.env.NEXT_PUBLIC_API_LOCAL_URL,
   headers: {
-    "Content-Type": "application/json",
+    'Content-Type': 'application/json',
   },
   withCredentials: true,
 });
 
 axiosInstance.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error.response?.status === 401 && typeof window !== "undefined") {
-      localStorage.removeItem("token");
-      toast.error("Session expired. Please log in again.");
-      window.location.href = "/signin";
+  response => response,
+  error => {
+    if (error.response?.status === 401 && typeof window !== 'undefined') {
+      localStorage.removeItem('token');
+      toast.error('Session expired. Please log in again.');
+      window.location.href = '/signin';
     }
     return Promise.reject(error);
   }
